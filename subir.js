daa0a5095ef2028a9430f7128853282296f698c8
jlalcazar <joseluisalcazar@gmail.com>
2015-01-13 23:23:41 +0100

    updated subir.js file

--- subir.js
+++ subir.js
@@ -1,15 +1,11 @@
-// Load the SDK and UUID
 var AWS = require('aws-sdk');
-
-AWS.config.update({accessKeyId: 'AKIAJAYMHJ4MKXZWLSHA', secretAccessKey: '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'});
-AWS.config.update({region: 'us-west-1'});
+aws.config.loadFromPath('./AwsConfig.json');
 
 
-// Create an S3 client
 var s3 = new AWS.S3();
 
 var bucketName = 'ehl1';
-var keyName = 'hello_world.txt';
+var keyName = 'hello1.txt';
 
   var params = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
   s3.putObject(params, function(err, data) {
