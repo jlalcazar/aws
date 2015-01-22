c216bee9ae20a51020a887eee3dfdd10c51d59b1
jlalcazar <joseluisalcazar@gmail.com>
2015-01-22 01:15:40 +0100

    Clean up error in upload.js

--- s3-upload.js
+++ s3-upload.js
@@ -1,15 +1,17 @@
+/*jshint -W117 */
 var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./AwsConfig.json');
 var s3 = new AWS.S3();
 
 var params = {
 	Bucket: 'cache.labutaca.net',
-	Key: 'test1.txt'
+	Key: 'test3.txt'
 };
 
-s3.putObject(params, function (err, data) {
-	if (err)
-		console.log(err)
-	else
-		console.log( "Successfully uploaded data " );
+s3.putObject(params, function (err) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log( "Successfully uploaded file " );
+	}
 });
